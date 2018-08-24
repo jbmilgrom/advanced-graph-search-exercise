9f016aa9324d4ff4d5560852cef33641c476fd90
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-24 12:20:16 -0700

    genericize queue comparator operation

--- priority_queue.py
+++ priority_queue.py
@@ -1,17 +1,18 @@
 class PriorityQueue:
 
-  def __init__(self):
+  def __init__(self, is_higher_priority = lambda subject, target: subject[0] < target[0]):
     self._high_to_low = [] # ordered from high to low so easier to pop off low from end
+    self._is_higher_priority = is_higher_priority
 
-  def insert(self, pair):
+  def insert(self, item):
     i = 0
     # TODO: divide and conquer to improve perforamce
     while i < len(self._high_to_low):
-      if pair[0] > self._high_to_low[i][0]:
-        self._high_to_low.insert(i, pair)
+      if not self._is_higher_priority(item, self._high_to_low[i]):
+        self._high_to_low.insert(i, item)
         return
       i += 1
-    self._high_to_low.append(pair)
+    self._high_to_low.append(item)
 
   def next(self):
     return self._high_to_low.pop()
