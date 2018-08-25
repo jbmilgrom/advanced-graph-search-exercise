417d0c370bcd58666be5358f98884477b6f88f76
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-24 17:08:39 -0700

    leverage pqueue for all search strategies

--- search.py
+++ search.py
@@ -2,7 +2,6 @@ import collections
 from constants import G, S, COSTS
 from priority_queue import PriorityQueue
 
-
 # Understanding:
 #   Find the shortest number of hops between S and G on a nxn grid
 #   return the path in the form of a dictionary {
@@ -15,76 +14,83 @@ from priority_queue import PriorityQueue
 #   Neighbors are N,S,E,W of every node
 #   Breadth first search of neighbors, until G is found
 #     tracking visited nodes by whether or not dest:source combination has occured before
+
 def bfs(grid, start, goal):
-  queue = collections.deque()
-  return no_cost_search(
+  return _no_cost_search(
     grid,
     start,
     goal,
-    lambda node: queue.append(node),
-    lambda: queue.popleft(),
-    lambda: len(queue) == 0
+    False
   )
 
 def dfs(grid, start, goal):
-  stack = collections.deque()
-  return no_cost_search(
+  return _no_cost_search(
     grid,
     start,
     goal,
-    lambda node: stack.append(node),
-    lambda: stack.pop(),
-    lambda: len(stack) == 0
+    True
   )
 
 def ucs(grid, start, goal):
-  length = len(grid)
-  visited = { start: 'START' }
-  pqueue = PriorityQueue()
-  pqueue.insert((0, start))
-  while not pqueue.is_empty():
-    cost, current = pqueue.next()
-    if grid[current[0]][current[1]] == G:
-        return visited
-    for next in neighbors(current, -1, length):
-      if visited.get(next):
-        continue
-      pqueue.insert((cost + COSTS[grid[next[0]][next[1]]], next))
-      visited[next] = current
+  return _cost_search(
+    grid,
+    start,
+    goal,
+    lambda _, __: 0
+  )
 
 def a_star(grid, start, goal):
+  return _cost_search(
+    grid,
+    start,
+    goal,
+    manhattan_distance_heuristic
+  )
+
+def _no_cost_search(grid, start, goal, insert_item_at_top_of_queue = False):
+  pqueue = PriorityQueue(lambda _, __: insert_item_at_top_of_queue)
+  return _search(
+    grid,
+    start,
+    goal,
+    lambda node: pqueue.insert(node),
+    lambda: pqueue.next(),
+    lambda: pqueue.is_empty(),
+    lambda coord: 1,
+    lambda _, __: 0
+  )
+
+def _cost_search(grid, start, goal, future_cost_heuristic):
+  pqueue = PriorityQueue()
+  return _search(
+    grid,
+    start,
+    goal,
+    lambda node: pqueue.insert(node),
+    lambda: pqueue.next(),
+    lambda: pqueue.is_empty(),
+    lambda coord: COSTS[grid[coord[0]][coord[1]]],
+    future_cost_heuristic
+  )
+
+def _search(grid, start, goal, enter, exit, is_empty, step_cost, future_cost_heuristic):
   length = len(grid)
   visited = { start: 'START' }
-  pqueue = PriorityQueue()
-  pqueue.insert((0 + manhattan_distance_heuristic(start, goal), 0, start))
-  while not pqueue.is_empty():
-    _, cost, current = pqueue.next()
+  enter((0 + future_cost_heuristic(start, goal), 0, start))
+  while not is_empty():
+    _, cost, current = exit()
     if grid[current[0]][current[1]] == G:
         return visited
     for next in neighbors(current, -1, length):
       if visited.get(next):
         continue
-      next_cost = cost + COSTS[grid[next[0]][next[1]]]
-      heuristic = next_cost + manhattan_distance_heuristic(next, goal)
-      pqueue.insert((heuristic, next_cost, next))
+      next_cost = cost + step_cost(next)
+      heuristic = next_cost + future_cost_heuristic(next, goal)
+      enter((heuristic, next_cost, next))
       visited[next] = current
 
 def manhattan_distance_heuristic(current, goal):
    return abs(goal[0] - current[0]) + abs(goal[1] - current[1])
-
-def no_cost_search(grid, start, goal, enter, exit, is_empty):
-  length = len(grid)
-  visited = { start: 'START' }
-  enter(start)
-  while not is_empty():
-    current = exit()
-    for next in neighbors(current, -1, length):
-      if visited.get(next):
-        continue
-      enter(next)
-      visited[next] = current
-      if grid[next[0]][next[1]] == G:
-        return visited
 
 def neighbors(coordinate, low, high):
   x, y = coordinate
